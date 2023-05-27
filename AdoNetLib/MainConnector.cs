2d92dad2a0ef3e2afa17ed96053a9f35339491e5
VasiliiChi <chibisowasya@gmail.com>
2023-05-27 11:57:25 +0300

    Добавьте файлы проекта.

---
+++ AdoNetLib/MainConnector.cs
@@ -0,0 +1,38 @@
+﻿using AdoNetLib.Configurations;
+using Microsoft.Data.SqlClient;
+
+namespace AdoNetLib
+
+{
+    public class MainConnector
+    {
+        
+        public async Task<bool> ConnectAsync()
+        {
+            bool result;
+            try
+            {
+                connection = new SqlConnection(ConnectionString.MsSqlConnection);
+                await connection.OpenAsync();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public async void DisconnectAsync()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                await connection.CloseAsync();
+            }
+        }
+
+
+
+    }
+}
---
+++ AdoNetModuleConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using AdoNetLib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var connector = new MainConnector();
+
+        var result = connector.ConnectAsync();
+
+        if (result.Result)
+        {
+            Console.WriteLine("Подключено успешно!");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка подключения!");
+        }
+
+        Console.ReadKey();
+
+    }
+}
