700f5a9e9c080f005fde5d416389dbd84da8175a
VasiliiChi <chibisowasya@gmail.com>
2023-05-28 14:17:17 +0300

    Реализовали отсоединенную модель

--- AdoNetModuleConsole/Program.cs
+++ AdoNetModuleConsole/Program.cs
@@ -1,16 +1,48 @@
 ﻿using AdoNetLib;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
         var connector = new MainConnector();
-
+        var data = new DataTable();
         var result = connector.ConnectAsync();
 
         if (result.Result)
         {
             Console.WriteLine("Подключено успешно!");
+
+            var db = new DbExecutor(connector);
+            var tablename = "NetworkUser";
+
+            Console.WriteLine("Получаем данные с таблицы: " + tablename);
+
+            data = db.SelectAll(tablename);
+            Console.WriteLine("Количество строк в " + tablename + ": " + data.Rows.Count);
+
+            Console.WriteLine("Отключаем БД!");
+            connector.DisconnectAsync();
+
+            Console.WriteLine("Количество строк в " + tablename + ": " + data.Rows.Count);
+
+            // Выводим наименование колонок
+            foreach (DataColumn column in data.Columns)
+            {
+                Console.Write($"{column.ColumnName}\t");
+            }
+            Console.WriteLine();
+
+            foreach (DataRow row in data.Rows)
+            {
+                var cells = row.ItemArray;
+                foreach (var cell in cells)
+                {
+                    Console.Write($"{cell}\t");
+                }
+                Console.WriteLine();
+            }
+
         }
         else
         {
